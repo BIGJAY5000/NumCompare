ed2aa50685dc4b8cb2a096808c09f4c8977d7946
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-28 09:40:50 -0400

    numbers

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int numOne;
-        int numTwo;
+        int numOne = 0;
+        int numTwo = 0;
         System.out.println("Please enter a number");
-        numOne = scan.nextInt();
-        System.out.println("Now please enter a second number");
-        numTwo = scan.nextInt();
-        if (numOne > numTwo) {
-            System.out.println("The first number, " + numOne + ", is greater than your second number, " + numTwo + ".");
+        if (scan.hasNextInt()) {
+            numOne = scan.nextInt();
+            System.out.println("Now please enter a second number");
+            if (scan.hasNextInt()) {
+                numTwo = scan.nextInt();
+                if (numOne > numTwo) {
+                    System.out.println("The first number, " + numOne + ", is greater than your second number, " + numTwo + ".");
+                } if (numOne < numTwo) {
+                    System.out.println("The first number, " + numOne + ", is less than your second number, " + numTwo + ".");
+                } if (numOne == numTwo) {
+                    System.out.println("The first number, " + numOne + ", is equal to your second number, " + numTwo + ".");
+                }
+            }
         }
-        else if (numOne < numTwo) {
-            System.out.println("The first number, " + numOne + ", is less than your second number, " + numTwo + ".");
-        }
-        else if (numOne == numTwo) {
-            System.out.println("The first number, " + numOne + ", is equal to your second number, " + numTwo + ".");
+        else {
+            System.out.println("You have entered an invalid integer value");
+            System.exit(0);
         }
 
     }
