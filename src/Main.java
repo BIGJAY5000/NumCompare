455f67a771c917a2a60df6614506517961addc93
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-28 09:30:27 -0400

    numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int numOne;
+        int numTwo;
+        System.out.println("Please enter a number");
+        numOne = scan.nextInt();
+        System.out.println("Now please enter a second number");
+        numTwo = scan.nextInt();
+        if (numOne > numTwo) {
+            System.out.println("The first number, " + numOne + ", is greater than your second number, " + numTwo + ".");
+        }
+        else if (numOne < numTwo) {
+            System.out.println("The first number, " + numOne + ", is less than your second number, " + numTwo + ".");
+        }
+        else if (numOne == numTwo) {
+            System.out.println("The first number, " + numOne + ", is equal to your second number, " + numTwo + ".");
+        }
+
     }
 }
